0dab84489b4f64c85b5e1e0c74715a169f685e35
Sleeplessgirl1 <rodriguezjacqueline01@outlook.com>
2026-01-23 09:31:09 -0600

    Update cars.ts

--- src/data/cars.ts
+++ src/data/cars.ts
@@ -242,37 +242,6 @@ export const cars: Car[] = [
     ]
   }
 },
-{
-  id: '76',
-  name: 'FORD MUSTANG GT',
-  year: 2024,
-  category: 'Deportivo',
-  price: 1090000,
-  image: '/cars/FORD MUSTANG GT 20241.webp',
-  images: [
-    '/cars/FORD MUSTANG GT 20241.webp',
-    '/cars/FORD MUSTANG GT 20242.webp',
-    '/cars/FORD MUSTANG GT 20243.webp',
-    '/cars/FORD MUSTANG GT 20244.webp',
-    '/cars/FORD MUSTANG GT 20245.webp',
-    '/cars/FORD MUSTANG GT 20246.webp',
-    '/cars/FORD MUSTANG GT 20247.webp',
-    '/cars/FORD MUSTANG GT 20248.webp',
-    '/cars/FORD MUSTANG GT 20249.webp'
-  ],
-  details: {
-    condition: 'Factura de agencia',
-    keys: '2 llaves',
-    kilometers: '6,229 Kms',
-    engine: 'V8 5.0 L',
-    features: [
-      'Llévatelo con un enganche de $218,000',
-      'Varios modos de manejo',
-      'XPEL',
-      'Precio Negociable'
-    ]
-  }
-},
   {
   id: '75',
   name: 'FORD LOBO PLATINUM',
