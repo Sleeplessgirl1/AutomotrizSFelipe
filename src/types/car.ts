f0e390ec0e7d8da3bae9a35ef47241853b47f10c
Sleeplessgirl1 <rodriguezjacqueline01@outlook.com>
2026-01-30 13:56:23 -0600

    up

--- src/types/car.ts
+++ src/types/car.ts
@@ -1,4 +1,4 @@
-export type CarCategory = 'Sedan' | 'Deportivo' | 'Pick Up' | 'Minivan' | 'SUV' ;
+export type CarCategory = 'Sedan' | 'Deportivo' | 'Pick Up' | 'SUV' ;
 
 export interface Car {
   id: string;
