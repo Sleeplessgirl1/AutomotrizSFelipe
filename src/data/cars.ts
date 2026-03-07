aaa9c5971586dcda12dd877ec6d6d41a5abbe390
Sleeplessgirl1 <rodriguezjacqueline01@outlook.com>
2026-03-07 12:43:48 -0600

    Update cars.ts

--- src/data/cars.ts
+++ src/data/cars.ts
@@ -759,7 +759,34 @@ images: [
     ]
   }
 },
-
+{
+  id: '65',
+  name: 'MINI COOPER SALT',
+  year: 2020,
+  category: 'Deportivo',
+  price: 310000,
+  image: '/cars/MINI COOPER SALT 20201.webp',
+  images: [
+    '/cars/MINI COOPER SALT 20201.webp',
+    '/cars/MINI COOPER SALT 20202.webp',
+    '/cars/MINI COOPER SALT 20203.webp',
+    '/cars/MINI COOPER SALT 20204.webp',
+    '/cars/MINI COOPER SALT 20205.webp',
+    '/cars/MINI COOPER SALT 20206.webp',
+    '/cars/MINI COOPER SALT 20207.webp',
+   
+  ],
+  details: {
+    condition: 'Factura de agencia',
+    keys: '2 llaves',
+    kilometers: '58,366 Kms',
+    engine: '3 cilindros 1.5 L',
+    features: [
+      'Financiamiento Disponible',
+      'Precio Negociable'
+    ]
+  }
+},
    {
   id: '63',
   name: 'BMW X6 M50i',
@@ -913,38 +940,7 @@ images: [
       ]
     }
   },
-  {
-    id: '50',
-    name: 'MINI Cooper S Multitone Edition',
-    year: 2023,
-    category: 'Deportivo',
-    price: 510000,
-    image: '/cars/MINI Cooper S Multitone Edition 20231.webp',
-    images: [
-      '/cars/MINI Cooper S Multitone Edition 20231.webp',
-      '/cars/MINI Cooper S Multitone Edition 20232.webp',
-      '/cars/MINI Cooper S Multitone Edition 20233.webp',
-      '/cars/MINI Cooper S Multitone Edition 20234.webp',
-      '/cars/MINI Cooper S Multitone Edition 20235.webp',
-      '/cars/MINI Cooper S Multitone Edition 20236.webp',
-      '/cars/MINI Cooper S Multitone Edition 20237.webp',
-      '/cars/MINI Cooper S Multitone Edition 20238.webp',
-      '/cars/MINI Cooper S Multitone Edition 20239.webp'
-    ],
-    details: {
-      condition: 'Factura de agencia (único dueño)',
-      keys: '2 llaves',
-      kilometers: '30,211 Kms',
-      engine: '4 cilindros Turbo',
-      features: [
-        'Financiamiento Disponible',
-        'Garantía hasta 2027',
-        'Edición especial (multitone)',
-        '2 juegos de molduras',
-        'Precio Negociable'
-      ]
-    }
-  },
+ 
    
    {
     id: '46',
