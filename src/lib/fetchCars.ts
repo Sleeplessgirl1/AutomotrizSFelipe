ba93fdd5277cc4ed7c1e75b3f1edd79dcdbab87c
Sleeplessgirl1 <rodriguezjacqueline01@outlook.com>
2026-05-21 16:09:51 -0600

    Fix imagenes

--- src/lib/fetchCars.ts
+++ src/lib/fetchCars.ts
@@ -2,16 +2,22 @@ import { client } from './sanityClient'
 import { Car } from '@/types/car'
 
 export async function fetchCars(): Promise<Car[]> {
-  return client.fetch(`
+  const data = await client.fetch(`
     *[_type == "auto"] | order(_createdAt desc) {
       "id": _id,
       name,
       year,
       category,
       price,
-      "image": images[0].asset->url,
-      "images": images[].asset->url,
+      "imageRaw": images[0]._sanityAsset,
+      "imagesRaw": images[]._sanityAsset,
       details
     }
   `)
+
+  return data.map((car: any) => ({
+    ...car,
+    image: car.imageRaw?.replace('image@', '') || '',
+    images: (car.imagesRaw || []).map((i: string) => i?.replace('image@', '') || ''),
+  }))
 }
