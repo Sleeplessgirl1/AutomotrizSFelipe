df18a86fb5f89c37d84088b3fbb517cf6018dc4a
Sleeplessgirl1 <rodriguezjacqueline01@outlook.com>
2026-05-26 17:08:03 -0600

    desactivar cdn cache

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: 'nnuyo5k9',
   dataset: 'production',
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2024-01-01',
 })
